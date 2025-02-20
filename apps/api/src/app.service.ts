8dea45c81224057ccf56e13d9a2fbd42ce6f8b7b
Bartosz <bartosz.burdalski@gmail.com>
2025-02-20 23:54:30 +0100

    Clean up

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -5,9 +5,6 @@ import { PrismaService } from './prisma.service';
 export class AppService {
   constructor(private readonly db: PrismaService) {}
   async getHello() {
-    const user = await this.db.user.findMany();
-    const xd = await this.db.test.findFirst();
-    console.log(user, xd);
     return 'Hello World!';
   }
 }
--- apps/api/src/prisma.service.ts
+++ apps/api/src/prisma.service.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '@repo/database';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    console.log('test');
     await this.$connect();
   }
 }
