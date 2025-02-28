9183ede3e64c979db48463c11665e468ced5a17e
Bartosz <bartosz.burdalski@gmail.com>
2025-02-28 19:54:42 +0100

    feat: remove unused PrismaService and related code from AppService

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "./prisma.service";
 
 @Injectable()
 export class AppService {
-  constructor(private readonly db: PrismaService) {}
   async getHello() {
-    const users = await this.db.user.findMany();
-    console.log(users);
     return "Hello World!";
   }
 }
